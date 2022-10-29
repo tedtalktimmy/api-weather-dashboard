e8ad32b8152832ce3a1103c1db23650537a69ce7
tedtalktimmy <frtntn007@gmail.com>
2022-10-29 18:18:47 -0400

    still working on the script.js

--- script.js
+++ script.js
@@ -6,11 +6,23 @@ var currentEl = document.querySelector('#currentWeather');
 var fiveDayEl = document.querySelector('#fiveDayForecast');
 var h3El = document.createElement('h3');
 
-var toJSON = function(res) {
+var toJSON = (res => {
   return res.json();
-};
+});
 
-var getWeather = function(data, city) {
+var displayBtn = (cities => {
+  var savedCity = JSON.parse(localStorage.getItem('cities')) || [];
+  previousBtn.innerHTML = null
+  currentEl.innerHTML = null
+  for (city of citySaved) {
+    buttonEl = document.createElement('button');
+    buttonEl.textContent = city;
+    buttonEl.className = 'btn btn primary';
+    previousBtn.appendChild(buttonEl);
+  }
+});
+
+var getWeather = (data, city => {
   var currentEl = document.querySelector('#currentWeather')
   var h3El = document.createElement('h3')
   var icon = data.current.weather[0].icon
@@ -28,11 +40,28 @@ var getWeather = function(data, city) {
   uviEl.textContent = 'uv index: ' + data.daily[0].uviEl
   
   currentEl.appendChild(h3El)
-  
   currentEl.appendChild(pEl)
   currentEl.appendChild(humidEl)
   currentEl.appendChild(uviEl)
   currentEl.appendChild(windEl)
 
+  if (data.daily[0].uvi >= 0 && data.daily[0].uvi <= 3) {
+    uviEl.classList.add('low')
+  } else if (data.daily[0].uvi >= 4 && data.daily[0].uvi <=6) {
+  } else if (data.daily[0].uvi >= 7 && data.daily[0].uvi <=8) {
+  } else if (data.daily[0].uvi >= 9 && data.daily[0].uvi <=10) {
+    uviEl.classList.add('veryHigh')
+  } else {
+    uviEl.classList.add('extreme')
+  }
 
-}
+
+  var fiveDayForecast = data.daily.slice(1,6);
+  fiveDayEl.innerHTML = null;
+  for (day of fiveDayForecast) {
+  date;
+  }
+
+
+
+})
